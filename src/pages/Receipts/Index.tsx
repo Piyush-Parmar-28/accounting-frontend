dafff771c33e5d22063a2f103866208844a02f05
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-12-26 09:17:51 +0530

    added add new button on index page.

--- src/pages/Receipts/Index.tsx
+++ src/pages/Receipts/Index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const ReceiptHome: React.FC<Props> = (props) => {
     const location = useLocation();
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     return (
         <Dashboard>
             <div className="px-4 sm:px-6 md:px-8 grid grid-cols-12 gap-4 mt-6">
@@ -19,21 +19,11 @@ const ReceiptHome: React.FC<Props> = (props) => {
                         type="button"
                         className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
                         onClick={() =>
-                            navigate(`${location.pathname}/add`)
-                        }
-                    >
-                        <Icon name="add" className="h-4 w-4 mr-2" />
-                        Add New
-                    </button>
-                    <button
-                        type="button"
-                        className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none mt-2"
-                        onClick={() =>
                             navigate(`${location.pathname}/list`)
                         }
                     >
                         <Icon name="add" className="h-4 w-4 mr-2" />
-                        Show Receipts
+                        Add New
                     </button>
                 </div>
             </div>
